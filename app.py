2d0f45c03f7199becb5befcd5128bd1ac2be3133
TalariSrinvas <srinivastalari201067@gmail.com>
2024-11-27 17:34:20 +0000

    frontend

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from os import pathconf_names
 from flask import Flask, request, render_template, redirect, url_for, session
 from database import engine
 from sqlalchemy.sql import text
-from database import add_product_to_db
+from database import add_product_to_db, add_orders_to_db
+
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
 
@@ -10,6 +11,28 @@ app.secret_key = "your_secret_key"
 def load_products_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM products"))
+        products = []
+        for row in result:
+            products.append(row._asdict())
+        return products
+
+
+def load_fproducts_from_db(uid):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM products WHERE fid=:uid"),
+                              {"uid": uid})
+        products = []
+        for row in result:
+            products.append(row._asdict())
+        return products
+
+
+def load_bproducts_from_db(uid):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(
+                "SELECT Products.url as img,Products.pname as name,Orders.quantity as qt,Orders.price as pri FROM Orders,Products WHERE Orders.fid=Products.fid AND Orders.pid=Products.pid AND Orders.status='Ordered' AND Orders.bid=:uid"
+            ), {"uid": uid})
         products = []
         for row in result:
             products.append(row._asdict())
@@ -37,9 +60,8 @@ def load_UserD_from_db():
 def load_ordered_from_db(uid):
     with engine.connect() as conn:
         result = conn.execute(
-            text(
-                "SELECT * FROM Orders WHERE status='Ordered' AND (fid=:uid OR bid=:uid)"
-            ), {"uid": uid})
+            text("SELECT * FROM Orders WHERE status='Ordered' AND fid=:uid"),
+            {"uid": uid})
         ordered = []
         for row in result:
             ordered.append(row._asdict())
@@ -71,14 +93,26 @@ def load_delivered_from_db(uid):
 
 
 @app.route("/")
+def home():
+    logged_in = "username" in session
+    return render_template('home.html', logged_in=logged_in)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
+
+
+@app.route("/login")
 def login():
     return render_template('login.html')
 
 
-@app.route("/home")
-def home():
-    products = load_products_from_db()
-    return render_template('home.html', products=products)
+@app.route("/about")
+def about():
+    logged_in = "username" in session
+    return render_template('about.html', logged_in=logged_in)
 
 
 @app.route("/SingUp")
@@ -91,23 +125,83 @@ def log():
     return render_template('log.html')
 
 
+@app.route("/service")
+def service():
+    return render_template('service.html')
+
+
+@app.route("/view")
+def view():
+    uid = session['uid']
+    orders= load_ordered_from_db(uid)
+    products = load_products_from_db()
+    users=load_UserD_from_db()
+    logged_in = "username" in session
+    return render_template('view.html', orders=orders, products=products, users=users, logged_in=logged_in)
+
+
+@app.route("/products")
+def products():
+    logged_in = "username" in session
+    products = load_products_from_db()
+    return render_template('products.html',
+                           products=products,
+                           logged_in=logged_in)
+
+
 @app.route("/add")
 def add():
     return render_template('add.html')
 
+
 @app.route("/Add", methods=["POST"])
 def Add():
-    data=request.form
+    data = request.form
     username = session["username"]
     uid = session["uid"]
-    add_product_to_db(data,username,uid)
+    add_product_to_db(data, username, uid)
     return redirect(url_for('dashboard'))
+
+
+@app.route("/product")
+def product():
+
+    pid = request.args.get('pid')
+    fid = request.args.get('fid')
+    if not pid:
+        return "Product ID not provided", 400
+    products = load_products_from_db()
+    try:
+        pid = int(pid)
+        fid = int(fid)
+    except ValueError:
+        return "Invalid Product ID", 400
+    users = load_UserD_from_db()
+    user = next((u for u in users if u["uid"] == fid), None)
+    pd = next((u for u in products if u["pid"] == pid), None)
+    if pd is None:
+        return "Product not found", 404
+    return render_template('product.html', pd=pd, user=user)
+
+
+@app.route('/placeorder', methods=['POST', 'GET', 'PUT'])
+def placeorder():
+
+    fid = request.args.get('fid')
+    pid = request.args.get('pid')
+    bid = session.get('uid')
+
+    quantity = request.form.get('orderQuantity')
+    total_price = request.form.get('totalPrice')
+    add_orders_to_db(fid, pid, bid, quantity, total_price)
+    return redirect(url_for("home"))
+
+
 @app.route("/dashboard")
 def dashboard():
     if "username" not in session:
         return redirect(url_for("login"))
 
-    products = load_products_from_db()
     username = session["username"]
     utype = session["utype"]
     uid = session["uid"]
@@ -121,6 +215,7 @@ def dashboard():
 
     if user:
         if utype == "Farmer":
+            products = load_fproducts_from_db(uid)
             return render_template("dashboard.html",
                                    user=user,
                                    products=products,
@@ -128,6 +223,14 @@ def dashboard():
                                    accepted=accepted,
                                    delivered=delivered)
         elif utype == "Buyer":
+            products = load_bproducts_from_db(uid)
+            return render_template("dashboard1.html",
+                                   user=user,
+                                   products=products,
+                                   ordered=ordered,
+                                   accepted=accepted,
+                                   delivered=delivered)
+        elif utype == "Admin":
             return redirect(url_for("home"))
 
     return redirect(url_for("login"))
--- database.py
+++ database.py
@@ -1,16 +1,16 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 
 engine = create_engine(
     "postgresql+psycopg2://root:bth63051zF4pptj6ytBjVofIewGkupaZ@dpg-csseg29u0jms73ea4kf0-a.oregon-postgres.render.com/dma_app?sslmode=require"
 )
 
-def add_product_to_db(data,username,uid):
+
+def add_product_to_db(data, username, uid):
 
     with engine.connect() as conn:
         query = text(
-            "INSERT INTO products(fname, fid, pname, quantity, price, dt) "
-            "VALUES(:fname, :fid, :pname, :quantity, :price, :dt)"
-        )
+            "INSERT INTO products(fname, fid, pname, quantity, price, dt,url) "
+            "VALUES(:fname, :fid, :pname, :quantity, :price, :dt,:url)")
         conn.execute(
             query,
             {
@@ -20,7 +20,23 @@ def add_product_to_db(data,username,uid):
                 "quantity": data["quantity"],
                 "price": data["price"],
                 "dt": data["date"],
+                "url": data['url']
             },
         )
         conn.commit()
-
+def add_orders_to_db(fid, pid, bid, quantity, total_price):
+    with engine.begin() as conn:  # Transactional connection
+        query = text(
+            "INSERT INTO Orders(fid, pid, bid, quantity, price, status) "
+            "VALUES(:fid, :pid, :bid, :quantity, :price, 'Ordered')"
+        )
+        conn.execute(
+            query,
+            {
+                "fid": fid,
+                "pid": pid,
+                "bid": bid,
+                "quantity": quantity,
+                "price": total_price,
+            },
+        )
